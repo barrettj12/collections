d5a11bf25c53f539df1c86cd949509957b46a11c
Jordan Barrett <jordan.barrett@canonical.com>
2022-12-10 23:55:37 +1100

    Add String type Closes #3

---
+++ internal/gen/doc.go
@@ -0,0 +1,5 @@
+/*
+This package contains code-generation tools, particularly those which generate
+methods on data types from standard library or experimental packages.
+*/
+package main
---
+++ string.go
@@ -0,0 +1,6 @@
+package collections
+
+// String is a wrapper around a string.
+type String string
+
+//go:generate go run ./internal/gen/strings.go
